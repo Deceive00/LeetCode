43246477f316e2b19ff2d8eec38716639ca9757a
Deceive00 <112562195+Deceive00@users.noreply.github.com>
2024-08-22 16:13:50 +0700

    Time: 7 ms (75.83%), Space: 9.9 MB (27.41%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> map;
+        
+        for(int i = 0 ; i < s.length() ; i++){
+            map[s[i]]++;
+        }
+        for(int i = 0 ; i < t.length() ; i++){
+            map[t[i]]--;
+        }
+
+        for (auto& it: map) {
+            if(it.second != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
