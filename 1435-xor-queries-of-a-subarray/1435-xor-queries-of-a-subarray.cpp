add814b718d882150ff00de428857a366ae51ee9
Deceive00 <112562195+Deceive00@users.noreply.github.com>
2024-09-13 09:56:11 +0700

    Time: 55 ms (78.82%), Space: 41.7 MB (84.53%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
@@ -8,11 +8,13 @@ public:
         for (int i = 1; i <= n; i++) {
             prefixXor[i] = prefixXor[i - 1] ^ arr[i - 1];
         }
+ 
         for(int i = 0 ; i < size ; i++){
             int left = queries[i][0];
             int right = queries[i][1];
             ans[i] = prefixXor[right + 1] ^ prefixXor[left];
         } 
+  
         return ans;  
     }
 };
