61dcb970fbc3a5033d5daee6fc37bb8b2808ea25
Deceive00 <112562195+Deceive00@users.noreply.github.com>
2024-09-18 12:50:09 +0700

    Time: 8 ms (48.02%), Space: 17.4 MB (14.46%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -13,7 +13,7 @@ public:
             strNums.push_back(to_string(n));
         }
         sort(strNums.begin() , strNums.end(), cmpr);
-        if(!strNums.empty() && strNums[0] == "0") return "0";
+        if(strNums.empty() || strNums[0] == "0") return "0";
         string ans = "";
         for(string s : strNums){
             ans += s;
