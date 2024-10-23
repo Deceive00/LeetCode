ba228854dda0e58b4456c0e9a4edd9e6aca13d3d
Eldrian Daniswara Giovanni <112562195+Deceive00@users.noreply.github.com>
2024-10-23 15:56:36 +0700

    Time: 0 ms (100%), Space: 7.9 MB (31.13%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> stack;
-        int len = s.length();
-        for(int i = 0 ; i < len ; i++){ 
-            if(s[i] == '(' || s[i] == '[' || s[i] == '{') {
-                stack.push(s[i]);
-                continue;
-            } 
-
+        stack<char> st;
+        for(int i = 0 ; i < s.size() ; i++){
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
+                st.push(s[i]);
+            }
             else {
-                if (stack.empty()) return false;
-                char pop = stack.top();
-
-                if((s[i] == ')' && pop != '(') ||
-                    (s[i] == ']' && pop != '[') || 
-                    (s[i] == '}' && pop != '{')){
-                    return false;
-                }
-
-                stack.pop();
-            }   
+                if(st.empty()) return false;
+                if(s[i] == ']' && st.top() != '[') return false;
+                if(s[i] == ')' && st.top() != '(') return false;
+                if(s[i] == '}' && st.top() != '{') return false;
+                st.pop();
+            }
         }
-        return stack.empty();
+        return st.empty();
     }
 };
