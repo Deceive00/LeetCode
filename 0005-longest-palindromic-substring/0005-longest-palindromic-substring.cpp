46bdb32985c5c6e6510bf4f6c8cc5e8e2d47ab0b
Eldrian Daniswara Giovanni <112562195+Deceive00@users.noreply.github.com>
2026-01-17 22:48:58 +0700

    Time: 11 ms (63.46%), Space: 9.3 MB (91.63%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,33 +1,6 @@
 class Solution {
 public:
-    int max = 0;
-    string maxString;
-    void dfs(string s, long left, long right) {
-        if (left >= right) return;
-        int leftIter = left, rightIter = right;
-        int isNotPalindrome = false;
-        while (leftIter < rightIter) {
-            if (s[leftIter] != s[rightIter]) {
-                isNotPalindrome = true;
-            }
-            leftIter++; rightIter--;
-        }
-        if (!isNotPalindrome) {
-            int length = right - left + 1;
-            if (length >= max) {
-                max = length;
-                maxString = s.substr(left, length);
-            }
-        }
-        dfs(s, left, right - 1);
-        dfs(s, left + 1, right);
-    }
-
     string longestPalindrome(string s) {
-        // int left = 0, right = s.length() - 1;
-        // maxString = s.substr(0, 1);
-        // dfs(s, left, right);
-        // return maxString;
 
         int maxLeft = 0, length = 1;
 
